d91872ea4dbfcd4b294cefb48cf4f33da435e525
suraj dhas <112482371+suraj0798@users.noreply.github.com>
2023-08-06 11:02:58 +0000

    server is running

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 import express from "express"
 import cors from "cors"
-import dotenv from "dotenv"
 
 const app = express()
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000
 
-app.listen({PORT}, () => {
+app.use(cors());
+
+app.listen(PORT, () => {
     console.log(`app is listen on port: ${PORT}`);
 })
